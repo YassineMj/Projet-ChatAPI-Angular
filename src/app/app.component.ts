5ad0de20473e2b7f49a52c745c92143d73b176fa
YassineMj <yassinmoujahid46@gmail.com>
2024-04-14 17:29:33 +0100

    s'inscrire - s'identifier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,40 +8,5 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class AppComponent {
 
-  constructor(private http: HttpClient) { }
-
-  apiUrl = 'https://api.openai.com/v1/chat/completions';
-
-  userInput: string = '';
-  chatHistory: string[] = [];
-
-  result: any;
-
-  messages: { role: string; content: string; }[] = [];
-
-  httpHeaders = new HttpHeaders().set("Authorization", "Bearer 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");
-
-  sendMessage() {
-    // Ajouter le message de l'utilisateur aux messages
-    this.messages.push({ role: "user", content: this.userInput });
-
-    // Construire le payload avec les messages actuels
-    const payload = {
-      model: "gpt-3.5-turbo",
-      messages: this.messages
-    };
-
-    this.http.post(this.apiUrl, payload, { headers: this.httpHeaders }).subscribe({
-      next: (resp) => {
-        this.result = resp;
-        // Ajouter la réponse de l'API à l'historique du chat
-        this.messages.push({ role: "system", content: this.result.choices[0].message.content });
-        // Réinitialiser l'entrée utilisateur
-        this.userInput = '';
-      },
-      error: (err) => {
-        console.error('Erreur lors de l\'envoi du message :', err);
-      }
-    });
-  }
+ 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,29 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { SinscrireComponent } from './Sinscrire/sinscrire/sinscrire.component';
+import { SidentifierComponent } from './Sidentifier/sidentifier/sidentifier.component';
+import { ChatComponent } from './Chat/chat/chat.component';
+
+import {AngularFireModule } from '@angular/fire/compat';
+import{AngularFireAuthModule} from '@angular/fire/compat/auth';
+import { environment } from 'src/environments/environment';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SinscrireComponent,
+    SidentifierComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule
 
   ],
   providers: [],
